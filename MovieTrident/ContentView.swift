5534509752e5d764507868c25c1893ad3b9767b0
Kalinski10 <52208012+kalinski10@users.noreply.github.com>
2021-12-09 21:16:00 +0100

    Initial Commit

---
+++ MovieTrident/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieTrident
+//
+//  Created by kalin's personal on 09/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieTrident/MovieTridentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTridentApp.swift
+//  MovieTrident
+//
+//  Created by kalin's personal on 09/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieTridentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
